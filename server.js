eca78f3faf68bc468b48037bb2ce97b53792ecf7
Liuqing650 <1006822621@qq.com>
2018-03-14 15:11:20 +0800

    修改为可执行文件

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ import ReactDOMServer from 'react-dom/server';
 import Hello from './src/components/hello';
 import Transmit from 'react-transmit';
 
-const port = 3000;
+const port = 8002;
+
 function handleRender(req, res) {
-  // 把 Hello 组件渲染成 HTML 字符串
   const html = ReactDOMServer.renderToString(<Hello />);
-  
-  Transmit.renderToString(Hello).then(({reactString, reactData}) => {
+  Transmit.renderToString(Hello).then(({ reactString, reactData }) => {
     fs.readFile('./src/helpers/index.html', 'utf8', function (err, data) {
       if (err) throw err;
-
+      // 把渲染后的 React HTML 插入到 div 中 
       const document = data.replace(/<div id="app"><\/div>/, `<div id="app"><section role="main" class="react-container"><div>${reactString}</div></section></div>`);
       const output = Transmit.injectIntoMarkup(document, reactData, ['/static/dist/client.js']);
 
+      // 把响应传回给客户端
       res.send(document);
     });
   });
